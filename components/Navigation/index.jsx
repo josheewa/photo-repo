9a2f29c5c6eb0a15d687bd5ce66df7d6969c01da
Joshua Chen <jcgter777@gmail.com>
2023-12-30 09:17:47 -0500

    Fixed mobile sidebar menu, consolidated nav

--- components/Navigation/index.jsx
+++ components/Navigation/index.jsx
@@ -1,18 +1,139 @@
-import { useState } from 'react'
-import Navbar from './Navbar'
-import Sidebar from './Sidebar'
+import React, { useEffect, useRef, useState } from 'react'
+import Logo from './Logo'
+import MenuItems from './MenuItems'
+import MobileMenuItems from './MobileMenuItems'
+import { menuItemsData } from './menuItemsData'
 
 const Navigation = () => {
-  // toggle sidebar
+  // Toggle sidebar
   const [isOpen, setIsOpen] = useState(false)
-  const toggle = () => {
-    setIsOpen(!isOpen)
-  }
+  const sideRef = useRef(null)
+  const buttonRef = useRef(null)
+
+  useEffect(() => {
+    let isMouseDownHandled = false
+    let isHandlingClick = false
+
+    const handleClick = (event) => {
+      // Skip touchstart if mousedown has already been handled
+      if (event.type === 'touchstart' && isMouseDownHandled) {
+        return
+      }
+
+      // Closes menu when clicking outside menu and button
+      if (
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target) &&
+        sideRef.current &&
+        !sideRef.current.contains(event.target)
+      ) {
+        // setIsOpen(false)
+        if (!isHandlingClick) {
+          setIsOpen(false)
+        }
+      }
+      // Toggles menu when menu is pressed
+      else if (buttonRef.current && buttonRef.current.contains(event.target)) {
+        if (!isHandlingClick) {
+          setIsOpen((prev) => !prev)
+          if (event.type === 'mousedown') {
+            isMouseDownHandled = true
+          }
+          isHandlingClick = true
+
+          // Reset the flag after a short delay
+          setTimeout(() => {
+            isHandlingClick = false
+          }, 100)
+        }
+      }
+    }
+
+    // Handle both click and touch events
+    document.addEventListener('mousedown', handleClick)
+
+    // Attach touch events only on mobile devices
+    if ('ontouchstart' in window || navigator.msMaxTouchPoints) {
+      document.addEventListener('touchend', handleClick)
+    }
+    // document.addEventListener('touchstart', handleClick)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClick)
+      document.removeEventListener('touchend', handleClick)
+    }
+  }, [])
+
+  // const calculateMenuPosition = () => {
+  //   if (buttonRef.current && sideRef.current) {
+  //     const buttonRect = buttonRef.current.getBoundingClientRect();
+  //     const windowHeight = window.innerHeight;
+  
+  //     const leftPosition = buttonRect.left + buttonRect.width;
+  //     const rightPosition = window.innerWidth - buttonRect.left;
+  //     const topPosition = buttonRect.top + window.scrollY; // Adjust for scroll position
+  
+  //     // Calculate the bottom position if needed
+  //     // const bottomPosition = windowHeight - buttonRect.bottom + window.scrollY;
+  
+  //     sideRef.current.style.left = `${leftPosition}px`;
+  //     sideRef.current.style.right = `${rightPosition}px`;
+  //     sideRef.current.style.top = `${topPosition}px`;
+  //     // sideRef.current.style.bottom = `${bottomPosition}px`; // Uncomment if using bottom
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   calculateMenuPosition()
+  //   window.addEventListener('resize', calculateMenuPosition)
+
+  //   return () => {
+  //     window.removeEventListener('resize', calculateMenuPosition)
+  //   }
+  // }, [])
+
   return (
-    <>
-      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} toggle={toggle} />
-      <Navbar toggle={toggle} />
-    </>
+    <div className="nav-container">
+      {/* Main navbar for desktop */}
+      <div className="sticky top-0 z-10 h-20 w-full bg-gray-800">
+        <div className="container mx-auto h-full px-4">
+          <div className="flex h-full items-center justify-between">
+            <Logo />
+
+            <ul className="nav menus hidden gap-x-6 text-xl text-white md:flex">
+              {menuItemsData.map((menu, index) => {
+                return <MenuItems items={menu} key={index} />
+              })}
+            </ul>
+            {/* Placeholder div for layout of navbar items, to be modified */}
+            <div></div>
+            <button type="button" className="inline-flex items-center md:hidden" ref={buttonRef}>
+              <svg
+                width="40"
+                height="40"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path d="M4 18L20 18" stroke="#fff" strokeWidth="2" strokeLinecap="round" />
+                <path d="M4 12L20 12" stroke="#fff" strokeWidth="2" strokeLinecap="round" />
+                <path d="M4 6L20 6" stroke="#fff" strokeWidth="2" strokeLinecap="round" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Sidebar/mobile navbar */}
+      <div className="mobile-nav ${isOpen ? 'block' : 'hidden'} text-black">
+        {isOpen && (
+          <ul className="menus" ref={sideRef}>
+            {menuItemsData.map((menu, index) => {
+              return <MobileMenuItems items={menu} key={index} />
+            })}
+          </ul>
+        )}
+      </div>
+    </div>
   )
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,30 +1,30 @@
 // import { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect, useRef } from "react";
-import Modal from "../components/Modal";
-import cloudinary from "../utils/cloudinary";
-import getBase64ImageUrl from "../utils/generateBlurPlaceholder";
-import { ImageProps } from "../utils/types";
-import { useLastViewedPhoto } from "../utils/useLastViewedPhoto";
-import Carousel from "../components/Carousel";
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect, useRef } from 'react'
+import Modal from '../components/Modal'
+import cloudinary from '../utils/cloudinary'
+import getBase64ImageUrl from '../utils/generateBlurPlaceholder'
+import { ImageProps } from '../utils/types'
+import { useLastViewedPhoto } from '../utils/useLastViewedPhoto'
+import Carousel from '../components/Carousel'
 
 const Home = ({ images }) => {
-  const router = useRouter();
-  const { photoId } = router.query;
-  const [lastViewedPhoto, setLastViewedPhoto] = useLastViewedPhoto();
+  const router = useRouter()
+  const { photoId } = router.query
+  const [lastViewedPhoto, setLastViewedPhoto] = useLastViewedPhoto()
 
-  const lastViewedPhotoRef = useRef(null);
+  const lastViewedPhotoRef = useRef(null)
 
   useEffect(() => {
     // This effect keeps track of the last viewed photo in the modal to keep the index page in sync when the user navigates back
     if (lastViewedPhoto && !photoId) {
-      lastViewedPhotoRef.current.scrollIntoView({ block: "center" });
-      setLastViewedPhoto(null);
+      lastViewedPhotoRef.current.scrollIntoView({ block: 'center' })
+      setLastViewedPhoto(null)
     }
-  }, [photoId, lastViewedPhoto, setLastViewedPhoto]);
+  }, [photoId, lastViewedPhoto, setLastViewedPhoto])
 
   return (
     <>
@@ -37,7 +37,7 @@ const Home = ({ images }) => {
           <Modal
             images={images}
             onClose={() => {
-              setLastViewedPhoto(photoId);
+              setLastViewedPhoto(photoId)
             }}
           />
         )}
@@ -50,12 +50,11 @@ const Home = ({ images }) => {
               as={`/p/${id}`}
               ref={id === Number(lastViewedPhoto) ? lastViewedPhotoRef : null}
               shallow
-              className="after:content after:shadow-highlight group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg"
-            >
+              className="after:content after:shadow-highlight group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg">
               <Image
                 alt=""
                 className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
-                style={{ transform: "translate3d(0, 0, 0)" }}
+                style={{ transform: 'translate3d(0, 0, 0)' }}
                 placeholder="blur"
                 blurDataURL={blurDataUrl}
                 src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
@@ -72,20 +71,20 @@ const Home = ({ images }) => {
       </main>
       <footer className="p-6 text-center text-white/80 sm:p-12"></footer>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
 
 export async function getStaticProps() {
   const results = await cloudinary.v2.search
     .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
-    .sort_by("public_id", "desc")
+    .sort_by('public_id', 'desc')
     .max_results(400)
-    .execute();
-  let reducedResults = [];
+    .execute()
+  let reducedResults = []
 
-  let i = 0;
+  let i = 0
   for (let result of results.resources) {
     reducedResults.push({
       id: i,
@@ -93,22 +92,22 @@ export async function getStaticProps() {
       width: result.width,
       public_id: result.public_id,
       format: result.format,
-    });
-    i++;
+    })
+    i++
   }
 
   const blurImagePromises = results.resources.map((image) => {
-    return getBase64ImageUrl(image);
-  });
-  const imagesWithBlurDataUrls = await Promise.all(blurImagePromises);
+    return getBase64ImageUrl(image)
+  })
+  const imagesWithBlurDataUrls = await Promise.all(blurImagePromises)
 
   for (let i = 0; i < reducedResults.length; i++) {
-    reducedResults[i].blurDataUrl = imagesWithBlurDataUrls[i];
+    reducedResults[i].blurDataUrl = imagesWithBlurDataUrls[i]
   }
 
   return {
     props: {
       images: reducedResults,
     },
-  };
+  }
 }
