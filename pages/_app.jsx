f6fd896bfee9d0ba7d51e86ae8985b8f0b4ed594
Joshua Chen <josheewa7@gmail.com>
2024-01-05 15:10:52 -0500

    Added toasts for email sent

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,15 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
 import Navigation from '../components/Navigation'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navigation />
       <Component {...pageProps} />
+      <ToastContainer />
     </>
   )
 }
--- pages/email.jsx
+++ pages/email.jsx
@@ -1,13 +1,18 @@
 import Head from 'next/head'
 import React, { useState } from 'react'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { VscClearAll } from 'react-icons/vsc'
 
 const EmailForm = () => {
-  const [formData, setFormData] = useState({
-    subject: '',
+  const initialFormData = {
     name: '',
     email: '',
+    subject: '',
     message: '',
-  })
+  }
+
+  const [formData, setFormData] = useState(initialFormData)
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value })
@@ -27,19 +32,24 @@ const EmailForm = () => {
       })
 
       if (response.ok) {
-        console.log('Email sent successfully!')
+        // Display success notification
+        toast.success('Email sent successfully!')
       } else {
-        console.error(
-          'Failed to send email. Server returned:',
-          response.status,
-          response.statusText
-        )
+        // Display error notification
         const responseData = await response.json()
-        console.error('Server response data:', responseData)
+        toast.error(
+          `Failed to send email. Server returned: ${response.status}. ${responseData.message}`
+        )
       }
     } catch (error) {
-      console.error('Error sending email:', error)
+      // Display error notification
+      toast.error(`Error sending email: ${error.message}`)
     }
+  }
+
+  const handleClear = () => {
+    // Reset the form data to the initial state
+    setFormData(initialFormData)
   }
 
   return (
@@ -54,7 +64,14 @@ const EmailForm = () => {
             <div className="flex w-full flex-row">
               <span className="mr-1">
                 <label>Name</label>
-                <input type="text" name="name" placeholder="Your name" onChange={handleChange} />
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Your name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  key="name"
+                />
               </span>
 
               <span className="ml-1">
@@ -63,7 +80,9 @@ const EmailForm = () => {
                   type="email"
                   name="email"
                   placeholder="Your e-mail address"
+                  value={formData.email}
                   onChange={handleChange}
+                  key="email"
                 />
               </span>
             </div>
@@ -74,19 +93,33 @@ const EmailForm = () => {
                 type="text"
                 name="subject"
                 placeholder="E-mail subject"
-                onChange={handleChange}></input>
+                value={formData.subject}
+                onChange={handleChange}
+                key="subject"
+              />
             </span>
             <span>
               <label>Message</label>
               <textarea
                 name="message"
                 placeholder="Type your message here"
+                value={formData.message}
                 onChange={handleChange}
+                key="message"
               />
             </span>
           </div>
-
-          <button type="submit">Submit</button>
+          <div className="buttons">
+            <button className="clear-btn" type="button" onClick={handleClear}>
+              <span>
+                <VscClearAll />
+              </span>
+              <span>Clear</span>
+            </button>
+            <button className="submit-btn" type="submit">
+              Submit
+            </button>
+          </div>
         </form>
       </div>
     </>
