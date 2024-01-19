c9bd9811936fa5d589c0283a5dfa3e0e179cf828
Joshua Chen <josheewa7@gmail.com>
2024-01-19 09:15:29 -0500

    Added tag carousel to tag pages

---
+++ components/TagCarousel.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react'
+import Image from 'next/image'
+import ImageGallery from 'react-image-gallery'
+
+const TagCarousel = ({ startingImagePublicId, tag, format }) => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const assetsData = await getAssets(startingImagePublicId, tag, format)
+        setData(assetsData)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    fetchData()
+  }, [startingImagePublicId, tag, format])
+
+  return <ImageGallery items={data} autoPlay={false} showPlayButton={false} slideDuration={250} />
+}
+
+export default TagCarousel
+
+const getAssets = async (startingImagePublicId, tag, format) => {
+  try {
+    // Fetch data from the Cloudinary JSON API
+    const response = await fetch(`https://res.cloudinary.com/ddaymbzcc/image/list/${tag}.json`)
+
+    // Check if the response is successful
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data: ${response.statusText}`)
+    }
+
+    // Parse the JSON data from the response
+    const data = await response.json()
+    // Find the index of the starting image
+    const startingImageIndex = data.resources.findIndex(
+      (asset) => asset.public_id === startingImagePublicId
+    )
+    // Rotate the array so that anything in front of the first image is placed at the end
+    const rotatedResources = [
+      ...data.resources.slice(startingImageIndex),
+      ...data.resources.slice(0, startingImageIndex),
+    ]
+
+    // Map the rotated array to the desired format
+    const rotatedMediaAssets = rotatedResources.map((asset) => ({
+      original: `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_pad,w_1500,h_1000,b_auto/${asset.public_id}.${asset.format}`,
+      thumbnail: `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_pad,w_150,h_100,b_auto/${asset.public_id}.${asset.format}`,
+    }))
+    return rotatedMediaAssets
+    // // Exclude the starting image
+    // const otherMediaAssets = data.resources
+    //   .filter((asset) => asset.public_id !== startingImagePublicId)
+    //   .map((asset) => ({
+    //     original: `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_pad,w_1500,h_1000,b_black/${asset.public_id}.${asset.format}`,
+    //     thumbnail: `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_pad,w_150,h_100,b_black/${asset.public_id}.${asset.format}`,
+    //   }))
+    // const startingImage = {
+    //   original: `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_pad,w_1500,h_1000,b_black/${startingImagePublicId}.${format}`,
+    //   thumbnail: `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_pad,w_150,h_100,b_black/${startingImagePublicId}.${format}`,
+    //   publicId: startingImagePublicId,
+    // }
+    // const mediaAssets = [startingImage, ...otherMediaAssets]
+    // return mediaAssets
+  } catch (error) {
+    console.error('Error fetching media assets:', error)
+    return []
+  }
+}
--- pages/tags/[tag].jsx
+++ pages/tags/[tag].jsx
@@ -3,18 +3,19 @@ import Image from 'next/image'
 import { useEffect, useRef, useState } from 'react'
 import cloudinary from '../../utils/cloudinary'
 import { IoMdClose } from 'react-icons/io'
+import TagCarousel from '../../components/TagCarousel'
 
 const Home = ({ images, tag }) => {
   const [openImage, setOpenImage] = useState(null)
-  const [modalImageSource, setModalImageSource] = useState(null)
-  const [isLandscape, setIsLandscape] = useState(false)
-  const imgRef = useRef(null)
+  const [openImageFormat, setOpenImageFormat] = useState(null)
+  const carouselRef = useRef(null)
   const modalRef = useRef(null)
 
   const handleImageClick = (public_id, format) => {
     setOpenImage(public_id)
+    setOpenImageFormat(format)
     setModalImageSource(
-      `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`
+      `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/${public_id}.${format}`
     )
   }
   const closeModal = () => {
@@ -23,7 +24,7 @@ const Home = ({ images, tag }) => {
   }
 
   const handleOutsideClick = (e) => {
-    if (imgRef.current && !imgRef.current.contains(e.target)) {
+    if (carouselRef.current && !carouselRef.current.contains(e.target)) {
       closeModal()
     }
   }
@@ -56,43 +57,29 @@ const Home = ({ images, tag }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main className="mx-auto max-w-[1960px] p-4">
-        {/* Popup image display */}
+        {/* Popup carousel display */}
         {openImage && (
           <>
             <div className="blur"></div>
             <div className="modal-container" ref={modalRef}>
-              <div className={`popup-image-container ${isLandscape ? 'landscape' : 'portrait'}`}>
+              <div className="popup-carousel-container" ref={carouselRef}>
                 <button className="close-btn" onClick={closeModal}>
                   <IoMdClose className="" />
-                  {/* × */}
                 </button>
-                <Image
-                  src={modalImageSource}
-                  layout="responsive"
-                  width={720}
-                  height={480}
-                  className="popup-image"
-                  sizes="(max-width: 640px) 100vw,
-                (max-width: 1280px) 50vw,
-                (max-width: 1536px) 33vw,
-                25vw"
-                  onLoad={handleImageLoad}
-                  ref={imgRef}
-                />
+                <TagCarousel startingImagePublicId={openImage} tag={tag} format={openImageFormat} />
               </div>
             </div>
           </>
         )}
         {images.length === 0 && (
-            <>
-              <div className="no-images-msg">
-                <span>No images associated with tag: </span>
-                <span className='tag'>{`${tag}`}</span>
-              </div>
-            </>
-          )}
+          <>
+            <div className="no-images-msg">
+              <span>No images associated with tag: </span>
+              <span className="tag">{`${tag}`}</span>
+            </div>
+          </>
+        )}
         <div className="img-container columns-2 gap-4 sm:columns-3 xl:columns-4 2xl:columns-5">
-          
           {images.length > 0 &&
             images.map(({ id, public_id, format }) => (
               <div
